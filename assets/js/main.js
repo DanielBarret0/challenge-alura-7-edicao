abf00b6c3fda0be4a976715a4d60e4c7f0045733
Daniel Barreto <99921965+DanielBarret0@users.noreply.github.com>
2023-06-23 12:20:12 -0300

    Responsividade tablet
    Foi adicionado a responsividade para tablet.

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-/* Funçao de abrir e fechar menu hamburguer */
-
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('open');
-});
-
