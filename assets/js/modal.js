7223aacccbb3b70018fb2f734db8d07ab0517029
Daniel Barreto <99921965+DanielBarret0@users.noreply.github.com>
2023-06-28 17:07:20 -0300

    Atializa'

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,31 +1,23 @@
-/* Modal */
 
-const botao = document.getElementById('botaoAbrirModal')
+function criarModal(botaoAbrirId, dialogId, botaoFecharId) {
+    const botao = document.getElementById(botaoAbrirId);
+    const modal = document.querySelector(dialogId);
+    const botaoFechar = document.getElementById(botaoFecharId);
 
-const modal = document.querySelector('#dialogCamiseta')
+    botao.onclick = function () {
+        modal.showModal();
+    };
 
-const botaoFechar = document.getElementById('botaoFechar')
-
-botao.onclick = function () {
-    modal.showModal()
+    botaoFechar.onclick = function () {
+        modal.close();
+    };
 }
 
-botaoFechar.onclick = function () {
-    modal.close()
-}
-
-/* Modal 2*/
-
-const botao2 = document.getElementById('botaoAbrirModal2')
-
-const modal2 = document.querySelector('#dialogCalca')
-
-const botaoFechar2 = document.getElementById('botaoFechar2')
-
-botao2.onclick = function () {
-    modal2.showModal()
-}
-
-botaoFechar2.onclick = function () {
-    modal2.close()
-}
+// Modais de cada card
+criarModal('botaoAbrirModal', '#dialogCamiseta', 'botaoFechar');
+criarModal('botaoAbrirModal2', '#dialogCalca', 'botaoFechar2');
+criarModal('botaoAbrirModal3', '#dialogTenis', 'botaoFechar3');
+criarModal('botaoAbrirModal4', '#dialogCasaco', 'botaoFechar4');
+criarModal('botaoAbrirModal5', '#dialogOculos', 'botaoFechar5');
+criarModal('botaoAbrirModal6', '#dialogBolsa', 'botaoFechar6');
+criarModal('botaoAbrirModal7', '#dialogNovidades', 'botaoFechar7');
