bf221c06f07dba0e0e081b0a233410b959b58e34
Daniel Barreto <99921965+DanielBarret0@users.noreply.github.com>
2023-07-11 16:09:03 -0300

    atualização semana 3
    Foi feito alguns recursos visuais como hover na categoria, efeito de fade no texto dos card.

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,4 +17,21 @@ function voltarAoTopo() {
     document.documentElement.scrollTop = 0; // Para outros navegadores
 }
 
+/* Efeito fade no texto */
 
+window.addEventListener('scroll', revelartexto);
+
+function revelartexto() {
+  var textElements = document.querySelectorAll('.revelar-texto p');
+  
+  for (var i = 0; i < textElements.length; i++) {
+    var element = textElements[i];
+    var elementTop = element.getBoundingClientRect().top;
+    var elementBottom = element.getBoundingClientRect().bottom;
+    var isVisible = elementTop < window.innerHeight && elementBottom >= 0;
+    
+    if (isVisible) {
+      element.classList.add('show');
+    }
+  }
+}
